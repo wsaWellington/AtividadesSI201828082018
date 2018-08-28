07c9ef511e0df97e6f4c568a48aaa3d4ddb364aa
Alunos <Alunos@BUTWKSLI7329.animaedu.intranet>
2018-08-28 20:53:10 -0300

    Initial commit

---
+++ app/src/main/java/br/usjt/sin/progmult/atividadessi2018/ActivityB.java
@@ -0,0 +1,13 @@
+package br.usjt.sin.progmult.atividadessi2018;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityB extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_b);
+    }
+}
---
+++ app/src/main/java/br/usjt/sin/progmult/atividadessi2018/ActivityC.java
@@ -0,0 +1,13 @@
+package br.usjt.sin.progmult.atividadessi2018;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityC extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_c);
+    }
+}
